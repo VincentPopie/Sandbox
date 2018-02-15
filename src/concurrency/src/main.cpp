3d1c2c83834d65e75c48cd7b1bc8ae74cf7a0eaf
Vincent Popie <vincent.popie@gmail.com>
2018-02-15 23:09:09 +1100

    Implemented a dot product and parrelize it

--- src/concurrency/src/main.cpp
+++ src/concurrency/src/main.cpp
@@ -1,5 +1,8 @@
+#include<atomic>
+#include<functional>
 #include<iostream>
 #include<thread>
+#include<vector>
 
 /*
  * Copy from
@@ -11,22 +14,81 @@ void call_from_thread(int tid) {
   std::cout << "Launched by thread " << tid << std::endl;
 }
 
-static const int num_threads = 10;
+std::vector<int> bounds(int nbr_splits, int nbr_elements){
+  std::vector<int> limits;
+
+  limits.push_back(0);
+  for (int i=1; i < nbr_splits; i++){
+    limits.push_back(nbr_elements/nbr_splits*i);
+  }
+  limits.push_back(nbr_elements);
+  return limits;
+}
+
+void dot_product_fail(const std::vector<int>& iVectorL,
+    const std::vector<int>& iVectorR,
+    int& oResult,
+    int lowerBound,
+    int upperBound) {
+
+  //int partialSum = 0;
+  for (int i = lowerBound; i < upperBound; i++) {
+    oResult += iVectorL[i] * iVectorR[i];
+  }
+  //oResult += partialSum;
+}
+
+void dot_product(const std::vector<int>& iVectorL,
+    const std::vector<int>& iVectorR,
+    std::atomic<int>& oResult,
+    int lowerBound,
+    int upperBound) {
+
+  int partialSum = 0;
+  for (int i = lowerBound; i < upperBound; i++) {
+    partialSum += iVectorL[i] * iVectorR[i];
+  }
+  oResult += partialSum;
+}
+
+
 
 int main() {
+  int nbr_elements = 100000;
+  int nbr_threads = 2;
+  int resultFail = 0;
+  std::atomic<int> result(0);
 
-  std::thread t[num_threads];
+  std::vector<std::thread> threads;
 
 
-  for (int i = 0; i < num_threads; ++i) {
-    t[i] = std::thread(call_from_thread,i);
+  std::vector<int> v1(nbr_elements,1), v2(nbr_elements,2);
+  std::vector<int> limits = bounds(nbr_threads, nbr_elements);
+
+
+  for (int i = 0; i < nbr_threads; i++) {
+    threads.push_back(std::thread(dot_product_fail,std::cref(v1), std::cref(v2), std::ref(resultFail), limits[i], limits[i+1]));
   }
 
-  std::cout << "Launched from the main\n";
 
-  for (int i = 0; i < num_threads; ++i) {
-    t[i].join();
+  for (auto& t : threads) {
+    t.join();
   }
+
+  std::cout<< "Result: " << resultFail << std::endl;
+  threads.clear();
+
+  for (int i = 0; i < nbr_threads; i++) {
+    threads.push_back(std::thread(dot_product, std::cref(v1), std::cref(v2), std::ref(result), limits[i], limits[i+1]));
+  }
+
+
+  for (auto& t : threads) {
+    t.join();
+  }
+
+  std::cout<< "Result: " << result << std::endl;
+
 
   return 0;
 }
