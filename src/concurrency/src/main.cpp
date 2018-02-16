848062856fe675fe8e3997f31078b147bc57a045
Vincent Popie <vincent.popie@gmail.com>
2018-02-16 18:51:18 +1100

    Refactor the code to avoid code duplication

--- src/concurrency/src/main.cpp
+++ src/concurrency/src/main.cpp
@@ -1,4 +1,5 @@
 #include<atomic>
+#include<chrono>
 #include<functional>
 #include<iostream>
 #include<thread>
@@ -9,11 +10,10 @@
  * https://solarianprogrammer.com/2011/12/16/cpp-11-thread-tutorial/
  */
 
-//This function will be called from a thread
-void call_from_thread(int tid) {
-  std::cout << "Launched by thread " << tid << std::endl;
-}
 
+/*
+ * Compute the begin and end indices of a given amount of subdivision of a vector
+ */
 std::vector<int> bounds(int nbr_splits, int nbr_elements){
   std::vector<int> limits;
 
@@ -25,22 +25,30 @@ std::vector<int> bounds(int nbr_splits, int nbr_elements){
   return limits;
 }
 
-void dot_product_fail(const std::vector<int>& iVectorL,
+
+/*
+ * Compute the dot product of a two vectors
+ * Not optimized for parallelization, as a locked resource is used in the loop
+ */
+template<typename T>
+void dotProduct(const std::vector<int>& iVectorL,
     const std::vector<int>& iVectorR,
-    int& oResult,
+    T& oResult,
     int lowerBound,
     int upperBound) {
-
-  //int partialSum = 0;
   for (int i = lowerBound; i < upperBound; i++) {
     oResult += iVectorL[i] * iVectorR[i];
   }
-  //oResult += partialSum;
 }
 
-void dot_product(const std::vector<int>& iVectorL,
+/*
+ * Compute the dot product of a two vectors
+ * Optimized for parallelization, as a locked resource is used only for an assignment
+ */
+template<typename T>
+void dotProductWithPartial(const std::vector<int>& iVectorL,
     const std::vector<int>& iVectorR,
-    std::atomic<int>& oResult,
+    T& oResult,
     int lowerBound,
     int upperBound) {
 
@@ -51,43 +59,79 @@ void dot_product(const std::vector<int>& iVectorL,
   oResult += partialSum;
 }
 
+/*
+ * Enum to call decide which function call
+ */
+enum class DotProduct {
+  DotProduct,
+  DotProductWithPartial
+};
+
+
+
+/*
+ * Test the dot Product in different case
+ */
+template <typename T>
+void testDotProduct(const std::vector<int>& v1,
+    const std::vector<int>& v2,
+    const std::vector<int>& limits,
+    int nbr_threads,
+    DotProduct algorithmChoice) {
+
+  T result(0);
+  std::vector<std::thread> threads;
+
+  auto t1 = std::chrono::system_clock::now();
+
+  std::function<void(const std::vector<int>& iVectorL,
+      const std::vector<int>& iVectorR,
+      T& oResult,
+      int lowerBound,
+      int upperBound)> dotProductCall;
+
+  if (algorithmChoice == DotProduct::DotProduct) {
+    dotProductCall = dotProduct<T>;
+  } else if (algorithmChoice == DotProduct::DotProductWithPartial)  {
+    dotProductCall = dotProductWithPartial<T>;
+  }
+
+
+  for (int i = 0; i < nbr_threads; i++) {
+    threads.emplace_back(std::thread(dotProductCall,std::cref(v1), std::cref(v2), std::ref(result), limits[i], limits[i+1]));
+  }
+
+
+  for (auto& t : threads) {
+    t.join();
+  }
+  auto t2 = std::chrono::system_clock::now();
+
+  std::cout<< "Result: " << result << std::endl;
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+  std::cout << "Duration: " << duration <<  " ms" << std::endl << std::endl;
+
+  threads.clear();
+}
+
+
 
 
 int main() {
-  int nbr_elements = 100000;
-  int nbr_threads = 2;
-  int resultFail = 0;
-  std::atomic<int> result(0);
 
-  std::vector<std::thread> threads;
-
+  int nbr_elements = 100000000;
+  int nbr_threads = std::thread::hardware_concurrency() ;
 
   std::vector<int> v1(nbr_elements,1), v2(nbr_elements,2);
   std::vector<int> limits = bounds(nbr_threads, nbr_elements);
 
+  std::vector<DotProduct> algorithms {DotProduct::DotProduct, DotProduct::DotProductWithPartial};
 
-  for (int i = 0; i < nbr_threads; i++) {
-    threads.push_back(std::thread(dot_product_fail,std::cref(v1), std::cref(v2), std::ref(resultFail), limits[i], limits[i+1]));
+  for (auto algorithm: algorithms) {
+    testDotProduct<int>(v1, v2, limits, nbr_threads, DotProduct::DotProduct);
+    testDotProduct<std::atomic<int>>(v1, v2, limits, nbr_threads, DotProduct::DotProduct);
   }
 
-
-  for (auto& t : threads) {
-    t.join();
-  }
-
-  std::cout<< "Result: " << resultFail << std::endl;
-  threads.clear();
-
-  for (int i = 0; i < nbr_threads; i++) {
-    threads.push_back(std::thread(dot_product, std::cref(v1), std::cref(v2), std::ref(result), limits[i], limits[i+1]));
-  }
-
-
-  for (auto& t : threads) {
-    t.join();
-  }
-
-  std::cout<< "Result: " << result << std::endl;
 
 
   return 0;
