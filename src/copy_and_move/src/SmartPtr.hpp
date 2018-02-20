0fb903c0db43630431f28be958a70aa2b0d90149
Vincent Popie <vincent.popie@gmail.com>
2018-02-20 14:28:23 +1100

    Moved the code from SmartPtr.cpp to SmartPtr.hpp (SmartPtr is a templated class)

--- src/copy_and_move/src/SmartPtr.hpp
+++ src/copy_and_move/src/SmartPtr.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstddef>
+#include <iostream>
 
 template <typename T>
 class SmartPtr {
@@ -9,17 +10,43 @@ public:
   explicit SmartPtr(T* iPtr): _ptr(iPtr) {};
 
   SmartPtr(const SmartPtr<T>& aCopy) = delete;
-  SmartPtr(SmartPtr<T>&& aMove);
-  SmartPtr& operator= (SmartPtr<T>&& aMove);
+
+  SmartPtr(SmartPtr<T>&& aMove) {
+    std::cout << "Call SmartPtr move constructor" << std::endl;
+    _ptr = aMove._ptr;
+    aMove._ptr = nullptr;
+  }
+
+  SmartPtr& operator= (SmartPtr<T>&& aMove){
+    std::cout << "Call SmartPtr move assignment operator" << std::endl;
+    if (_ptr != nullptr) {
+      delete _ptr;
+    }
+
+    _ptr = aMove._ptr;
+    aMove._ptr = nullptr;
+    return *this;
+  }
+
   T* operator->() const
       { return _ptr; }
-  ~SmartPtr();
+
+  ~SmartPtr() {
+    std::cout << "Call SmartPtr destructor...";
+    if (_ptr != nullptr) {
+      std::cout << " foo is not null, it will be destructed" << std::endl;
+      delete _ptr;
+    } else {
+      std::cout << " foo is null" << std::endl;
+    }
+  }
 
   bool operator!= (const SmartPtr<T>& compare) const {
     return (_ptr != compare._ptr);
   }
 
-  bool operator!= (const std::nullptr_t compare) const {
+  template <typename Y>
+  bool operator!= (const Y compare) const {
     return  (_ptr != compare);
   }
 
@@ -28,6 +55,3 @@ private:
   T* _ptr;
 
 };
-
-#include "SmartPtr.cpp"
-
